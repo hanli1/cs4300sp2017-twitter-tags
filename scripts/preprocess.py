975226468801635cab918813a17125fcdde0363e
Han Li <hl565@cornell.edu>
2017-03-29 14:03:05 -0400

    cleaned up preprocess.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,9 +1,9 @@
 # -*- coding: UTF-8 -*-
-# from __future__ import unicode_literals
 import re
 import sys
 import csv
-# import codecs
+
+#u use utf8 by default
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -15,7 +15,7 @@ emoji_pattern = re.compile(
     u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
     "+", flags=re.UNICODE)
 
-new_file = open("processed_tweets.csv", "wb")
+processed_file = open("processed_tweets.csv", "wb")
 
 entire_text = []
 with open('user_tweets.csv','rb') as f:
@@ -23,21 +23,11 @@ with open('user_tweets.csv','rb') as f:
   for a, b, line in reader:
     content =  re.sub(r'http\S+', '', line, flags=re.MULTILINE) # remove links
     content = content.decode('utf-8')
-    # print content[:200]
-    #\\U0001f(.*?)(?=[A-Za-z0-9_-])
-    # text = u"hello 😔 🔥 🌱 world".encode('unicode-escape')
-    # print text
-    # print re.sub(emoji_pattern, "", text)
-    content = re.sub(emoji_pattern, "", content) # no emoji
-    # content = content.replace("??", "").replace("?", "")
-    # print content[:200]
-    # content = content.decode('unicode-escape')
-    # new_file.write(content)
+    content = re.sub(emoji_pattern, "", content) # remove emojis
     entire_text.append([a, b, content])
-    # content = re.sub("[…]? http\S+", "", content) # remove links
 
-writer = csv.writer(new_file)
+writer = csv.writer(processed_file)
 writer.writerows(entire_text)
-new_file.close()
+processed_file.close()
 
 
