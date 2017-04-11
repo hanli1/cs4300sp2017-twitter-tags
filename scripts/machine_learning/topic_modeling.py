35f46cd9c865a1c1138687ccad328adbbb7dc13d
Kevin Jiang <kzj1995@yahoo.com>
2017-04-10 21:18:50 -0400

    Implement seed words LDA

--- scripts/machine_learning/topic_modeling.py
+++ scripts/machine_learning/topic_modeling.py
@@ -9,12 +9,10 @@ import sys
 
 processed_directory = "../../data/processed_tweets"
 
-class BasicLDA:
+class LDASuperClass:
     """
-    Implements training and loading of the basic LDA model given tokenized documents
-    and a list of the users
+    Super class for any LDA implementations
     """
-    
     def __init__(self, all_tokenized_documents, all_processed_users):
         """
         Performs initializations necessary for the LDA model; specifically, prepares
@@ -30,20 +28,29 @@ class BasicLDA:
             self.bag_of_words_dict[all_processed_users[i]] = self.bag_of_words_documents[i]
         self.lda_model = None
 
+class BasicLDA(LDASuperClass):
+    """
+    Implements training and loading of the basic LDA model given tokenized documents
+    and a list of the users
+    """
+
+    def __init__(self, all_tokenized_documents, all_processed_users):
+        LDASuperClass.__init__(self, all_tokenized_documents, all_processed_users)
+
     def train_basic_lda(self, num_topics, num_passes):
         """
-        Given a list of tokenized documents, performs LDA on these documents
+        Performs basic LDA with the given number of topics and given number of passes
         """
         #Enable logging in order to track the process of the LDA model
         logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-        lda_model = gensim.models.ldamodel.LdaModel(self.bag_of_words_documents, \
+        self.lda_model = gensim.models.ldamodel.LdaModel(self.bag_of_words_documents, \
             num_topics=num_topics, id2word = self.dictionary, passes=num_passes)
-        lda_model.save('trained_basic_lda_model/lda.model')
-        topics_distribution = lda_model.show_topics(num_topics=2, num_words=20)
+        self.lda_model.save('trained_basic_lda_model/lda.model')
+        topics_distribution = self.lda_model.show_topics(num_topics=num_topics, num_words=20)
         with open("trained_basic_lda_model/topics_distribution", 'wb') as f:
-            for topic_distribution in topics_distribution:
-                f.write(str(topic_distribution[0]) + " ")
-                topic_words = topic_distribution[1].split("+")
+            for topic in topics_distribution:
+                f.write(str(topic[0]) + " ")
+                topic_words = topic[1].split("+")
                 for topic_word in topic_words:
                     f.write(topic_word.strip() + " ")
                 f.write("\n")
@@ -52,10 +59,75 @@ class BasicLDA:
         """
         Loads a trained LDA model from disk
         """
-        lda_model = gensim.models.ldamodel.LdaModel.load('trained_basic_lda_model/lda.model')
+        self.lda_model = gensim.models.ldamodel.LdaModel.load('trained_basic_lda_model/lda.model')
         print(self.lda_model[bag_of_words_dict["Ariana Grande"]])
         print(self.lda_model[bag_of_words_dict["Donald J. Trump"]])
 
+class SeedWordsLDA(LDASuperClass):
+    """
+    Implements training and loading of an LDA model that first adjusts the 
+    topic-word probability distributions based on given seed words specified 
+    for each topic
+    """
+
+    def __init__(self, all_tokenized_documents, all_processed_users):
+        LDASuperClass.__init__(self, all_tokenized_documents, all_processed_users)
+
+    def build_topic_word_distributions(self, seed_words_matrix):
+        """
+        Helper method for building the topic word probability distribution for each topic
+        based on the given seed words matrix parameter (seed words for each topic), and 
+        these distributions will be provided as a parameter to the LDA training
+        """
+        topic_word_distributions = []
+        num_words = len(self.dictionary.keys())
+        for i in range(len(seed_words_matrix)):
+            topic_seed_words = seed_words_matrix[i]
+            topic_word_distribution = [0] * num_words
+            seed_words_prob_sum = 0.0
+            seed_words_count = 0
+            for word_id in self.dictionary.keys():
+                if self.dictionary.get(word_id) in topic_seed_words:
+                    topic_word_distribution[word_id] = 3.0 / num_words
+                    seed_words_prob_sum = seed_words_prob_sum + (3.0 / num_words)
+                    seed_words_count = seed_words_count + 1
+            regular_words_prob_sum = 1.0 - seed_words_prob_sum
+            regular_words_count = num_words - seed_words_count
+            for word_id in self.dictionary.keys():
+                if self.dictionary.get(word_id) not in topic_seed_words:
+                    topic_word_distribution[word_id] = regular_words_prob_sum / regular_words_count
+            topic_word_distributions.append(topic_word_distribution)
+        return topic_word_distributions
+
+    def train_seed_words_lda(self, num_topics, num_passes, seed_words_matrix):
+        """
+        Performs LDA by first adjusting topic-word probability distributions using the
+        given seed_words matrix, and then trains with the given number of topics and
+        given number of passes
+        """
+        topic_word_distributions = self.build_topic_word_distributions(seed_words_matrix)
+        #Enable logging in order to track the process of the LDA model
+        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+        self.lda_model = gensim.models.ldamodel.LdaModel(self.bag_of_words_documents, \
+            eta=topic_word_distributions, num_topics=num_topics, id2word = self.dictionary, \
+            passes=num_passes)
+        self.lda_model.save('trained_seed_words_lda_model/lda.model')
+        topics_distribution = self.lda_model.show_topics(num_topics=num_topics, num_words=20)
+        with open("trained_seed_words_lda_model/topics_distribution", 'wb') as f:
+            for topic in topics_distribution:
+                f.write(str(topic[0]) + " ")
+                topic_words = topic[1].split("+")
+                for topic_word in topic_words:
+                    f.write(topic_word.strip() + " ")
+                f.write("\n")
+
+    def load_seed_words_lda(self):
+        """
+        Loads a trained LDA model from disk
+        """
+        self.lda_model = gensim.models.ldamodel.LdaModel.load('trained_seed_words_lda_model/lda.model')
+        print(self.lda_model[bag_of_words_dict["Ariana Grande"]])
+        print(self.lda_model[bag_of_words_dict["Donald J. Trump"]])
 
 def build_tokenized_documents_list(file_name):
     """
@@ -89,6 +161,10 @@ if __name__ == "__main__":
         train_or_load = sys.argv[1]
     except Exception as e:
         train_or_load = "train"
+    try:
+        lda_model_type = sys.argv[2]
+    except Exception as e:
+        lda_model_type = "seed_words"
     #Will contain all tokenized documents, where each document corresponds to all
     #the tweets of the user
     all_tokenized_documents = []
@@ -99,18 +175,35 @@ if __name__ == "__main__":
             processed_users, tokenized_documents = build_tokenized_documents_list(filename)
             all_tokenized_documents = all_tokenized_documents + tokenized_documents
             all_processed_users = all_processed_users + processed_users
-    basic_lda_model = BasicLDA(all_tokenized_documents, all_processed_users)
     if train_or_load == "train":
         try:
             #Number of topics to train from in the LDA model
-            num_topics = int(sys.argv[2])
+            num_topics = int(sys.argv[3])
         except Exception as e:
             num_topics = 20
         try:
             #Number of iterations/passes to run for in the LDA model
-            num_passes = int (sys.argv[3])
+            num_passes = int (sys.argv[4])
         except Exception as e:
             num_passes = 200
-        basic_lda_model.train_basic_lda(num_topics, num_passes)
+        if lda_model_type == "basic":
+            basic_lda_model = BasicLDA(all_tokenized_documents, all_processed_users)
+            basic_lda_model.train_basic_lda(num_topics, num_passes)
+        else if lda_model_type == "seed_words":
+            seed_matrix = []
+            with open("seed_words.txt", 'r') as f:
+                content = f.readlines()
+                for line in content:
+                    topic_seed_words = topic_seed_words[(line.index(":") + 1):].split(",")
+                    topic_seed_words = [word.strip() for word in topic_seed_words]
+                    seed_matrix.append(topic_seed_words)
+            seed_words_lda_model = SeedWordsLDA(all_tokenized_documents, all_processed_users)
+            seed_words_lda_model.train_seed_words_lda(num_topics, num_passes, seed_matrix)
     elif train_or_load == "load":
-        basic_lda_model.load_basic_lda()
+        if lda_model_type == "basic":
+            basic_lda_model = BasicLDA(all_tokenized_documents, all_processed_users)
+            basic_lda_model.load_basic_lda()
+        else:
+            seed_words_lda_model = SeedWordsLDA(all_tokenized_documents, all_processed_users)
+            seed_words_lda_model.load_seed_words_lda()
+
