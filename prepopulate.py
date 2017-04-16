14b5ad120d40afa8b08d443ddb80ac44b2c13325
Han Li <hl565@cornell.edu>
2017-04-16 16:32:33 -0400

    prepopulation of db

---
+++ prepopulate.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import io
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def populate():
+  location = "scripts\\machine_learning\\trained_naive_bayes_model\\user_tags.txt"
+  with io.open(location, 'r', encoding="ISO-8859-1") as f:
+    # test = "Sanders: democrat\nTrump: republican\nObama: democrat"
+    rows = f.read().split("\n")[:-1] #ignore last empty line
+    for row in rows:
+      items = row.split(": ")
+      name = items[0]
+      tags = items[1].split(" ")
+      for tag in tags:
+        if tag:
+          p = UserTag.objects.get_or_create(name=unicode(name), category=tag)[0]
+          print "added: " + str(p)
+
+  
+
+# Start execution here!
+if __name__ == '__main__':
+  print "Starting prepopulation script..."
+  os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+  import django
+  django.setup()
+
+  from project_template.models import UserTag
+  #purge db
+  UserTag.objects.all().delete()
+  populate() 
+
+  #retrieve some stuff
+  cat = list(UserTag.objects.filter(category="fashion_lover").values("name"))
+  cat = [i["name"] for i in cat]
+  print cat  
--- project_template/models.py
+++ project_template/models.py
@@ -5,3 +5,10 @@ from django.db import models
 # Create your models here.
 class Docs(models.Model):
 	address = models.CharField(max_length=200)
+
+class UserTag(models.Model):
+    name = models.CharField(max_length=128)
+    category = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.name + " " + self.category
