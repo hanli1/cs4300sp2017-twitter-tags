b229d703b3de757b6a88ad01a1dc174d057bd42c
Leo Tang <lt342@cornell.edu>
2017-04-23 02:10:37 -0400

    Improved Naive bayes model and created tags v3

--- scripts/machine_learning/Naive_Bayes_SVM.py
+++ scripts/machine_learning/Naive_Bayes_SVM.py
@@ -45,16 +45,12 @@ def train_data(data):
     Perform K-Fold cross validation and create full model
     """
     tweets = np.array(data[4])
-    y = np.array(data["CATEGORY"])
 
-    #Creating a index to category and category to index mapping
-    idx_to_cat = list(pd.Series(y, dtype="category").cat.categories)
-    cat_to_idx = {}
-    for i, cat in enumerate(idx_to_cat):
-        cat_to_idx[cat] = i
+    y = np.array(data["CATEGORY"])
 
     print "Performing 10-fold Cross Validation" 
     kf = KFold(len(tweets), n_folds = 10, shuffle=True)    
+
     vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None)
 
     accuracy = []
@@ -64,25 +60,12 @@ def train_data(data):
           
         nb = MultinomialNB()
         nb.fit(X_train, y[train_idx])
-        
-        llf = svm.LinearSVC()
-        llf.fit(X_train, y[train_idx])
 
         pred_nb = nb.predict(X_test)
         pred_nb_prob = nb.predict_proba(X_test)
-        
-        pred_svm = llf.predict(X_test)
-                
-        pred = pred_svm
+                        
+        pred = pred_nb
         pred_prob = pred_nb_prob
-        for i, row in enumerate(pred_nb_prob):
-            if np.max(row) >= 0.95:
-                pred[i] = pred_nb[i]
-            else:
-                svm_prob = [0]*NUM_CATEGORY;
-                svm_prob[cat_to_idx[pred_svm[i]]] = 1
-                pred_prob[i] = np.add(np.multiply(0.5, row), np.multiply(0.5,svm_prob))
-                pred[i] = idx_to_cat[np.argmax(pred_prob[i])]
                 
         accuracy.append(np.mean(pred == np.array(y[test_idx])))
         print "."
@@ -97,23 +80,57 @@ def train_data(data):
     nb = MultinomialNB()
     nb.fit(X, y)
 
-    llf = svm.LinearSVC()
-    llf.fit(X, y)
-
     with open("trained_naive_bayes_model/trained_model_v2", 'w') as f:
         pickle.dump(nb, f)
-
-    with open("trained_svm_model/trained_model_v2", 'w') as f:
-        pickle.dump(llf, f)
 
     with open('pickles/vectorizer', 'w') as f:
         pickle.dump(vectorizer, f)
 
-    # with open("trained_svm_model/trained_model_v2", 'r') as f:
-    #   llf = pickle.load(f)
+def train_agg_data(data):
+    """
+    Perform K-Fold cross validation and create full model
+    """
+    tweets = np.array(data['Tweets'])
 
-    # pred = llf.predict(X)
-    # print np.mean(pred == np.array(y))
+    y = np.array(data["CATEGORY"])
+
+    print "Performing 10-fold Cross Validation" 
+    kf = KFold(len(tweets), n_folds = 10, shuffle=True)    
+
+    vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = 'english')
+
+    accuracy = []
+    for train_idx, test_idx in kf:
+        X_train = vectorizer.fit_transform(tweets[train_idx])
+        X_test = vectorizer.transform(tweets[test_idx])
+          
+        nb = MultinomialNB()
+        nb.fit(X_train, y[train_idx])
+
+        pred_nb = nb.predict(X_test)
+        pred_nb_prob = nb.predict_proba(X_test)
+                        
+        pred = pred_nb
+        pred_prob = pred_nb_prob
+                
+        accuracy.append(np.mean(pred == np.array(y[test_idx])))
+        print "."
+
+    print ""
+    print "Avg Accuracy: " + str(np.mean(accuracy))
+    print accuracy
+
+    #Creating Full Model
+    X = vectorizer.fit_transform(tweets)
+
+    nb = MultinomialNB()
+    nb.fit(X, y)
+
+    with open("trained_naive_bayes_model/trained_agg_model_v2", 'w') as f:
+        pickle.dump(nb, f)
+
+    with open('pickles/vectorizer_agg', 'w') as f:
+        pickle.dump(vectorizer, f)
 
 def test_data(data):
     """
@@ -123,9 +140,6 @@ def test_data(data):
 
     with open("trained_naive_bayes_model/trained_model_v2", 'r') as f:
         nb = pickle.load(f)
-
-    with open("trained_svm_model/trained_model_v2", 'r') as f:
-        llf = pickle.load(f)
 
     with open('pickles/vectorizer', 'r') as f:
         vectorizer = pickle.load(f)
@@ -138,23 +152,29 @@ def test_data(data):
 
     X = vectorizer.transform(tweets)
 
-    pred_nb = nb.predict(X)
-    pred_nb_prob = nb.predict_proba(X)
-    pred_svm = llf.predict(X)        
-    pred = pred_svm
-    pred_prob = pred_nb_prob
-    for i, row in enumerate(pred_nb_prob):
-        if np.max(row) >= 0.95:
-            pred[i] = pred_nb[i]
-        else:
-            svm_prob = [0]*NUM_CATEGORY;
-            svm_prob[cat_to_idx[pred_svm[i]]] = 1
-            pred_prob[i] = np.add(np.multiply(0.5, row), np.multiply(0.5,svm_prob))
-            pred[i] = idx_to_cat[np.argmax(pred_prob[i])]
+    pred = nb.predict(X)
 
     return pred, idx_to_cat, cat_to_idx
 
-def tag_users(data, pred, idx_to_cat, cat_to_idx):
+def test_agg_data(data):
+    """
+    Returns prediction given X (data) as well mapping of index to category and vice versa
+    """
+    tweets = np.array(data['Tweets'].values.astype('U'))
+
+    with open("trained_naive_bayes_model/trained_agg_model_v2", 'r') as f:
+        nb = pickle.load(f)
+
+    with open('pickles/vectorizer_agg', 'r') as f:
+        vectorizer = pickle.load(f)
+    
+    X = vectorizer.transform(tweets)
+
+    pred_agg = nb.predict(X)
+
+    return pred_agg
+
+def tag_users(data, pred, pred_agg, idx_to_cat, cat_to_idx):
     """
     Tags untagged users
     """
@@ -164,16 +184,55 @@ def tag_users(data, pred, idx_to_cat, cat_to_idx):
         freq[cat_to_idx[pred[i]]]+=1
         users_tag_freq[row[1]] = freq
 
-    threshold = 0.65
-    with open("trained_naive_bayes_model/user_tags_v2", 'w') as f:
+    threshold = 0.3
+    with open("trained_naive_bayes_model/user_tags_v3", 'w') as f:
+        i = 0
         for user, freq in users_tag_freq.items():
             user_output = str(user) + ": "
-            user_output = user_output + str(idx_to_cat[np.argmax(freq)]) + " "
-            for i, c in enumerate(freq):
-                if c > np.max(freq)*threshold and i != np.argmax(freq):
-                    user_output = user_output + str(idx_to_cat[i]) + " "
+            # user_output = user_output + str(idx_to_cat[np.argmax(freq)]) + " "
+            user_output = user_output + str(pred_agg[i]) + " "
+            for j, c in enumerate(freq):
+                if c > np.sum(freq)*threshold and idx_to_cat[j] != pred_agg[i]:
+                    if (pred_agg[i] == "liberal" and idx_to_cat[j] == "conservative") or (pred_agg[i] == "conservative" and idx_to_cat[j] == "liberal"):
+                        pass
+                    user_output = user_output + str(idx_to_cat[j]) + " "
+            i+=1
             f.write(user_output + "\n")
     return
+
+def aggregate_tweets(data, train = True):
+    text = ""
+    users = []
+    tweets = []
+    category = []
+    if train:
+        for index, row in data.iterrows():
+            if len(users) == 0:
+                users.append(row[1])
+                if train:
+                    category.append(row[5])
+            elif users[-1] != row[1]:
+                users.append(row[1])
+                if train:
+                    category.append(row[5])
+                tweets.append(text)
+                text = ""
+            text = text + row[4] + " " 
+        tweets.append(text)
+        return pd.DataFrame({'User': users, 'Tweets': tweets, 'CATEGORY': category})
+    else:
+        for index, row in data.iterrows():
+            if len(users) == 0:
+                users.append(row[1])
+            elif users[-1] != row[1]:
+                users.append(row[1])
+                tweets.append(text)
+                text = ""
+            text = text + str(row[2]) + " "
+            if index % 100000 == 0:
+                print "Still aggregating..."
+        tweets.append(text)
+        return pd.DataFrame({'User': users, 'Tweets': tweets})
 
 if __name__ == "__main__":
     try:
@@ -185,12 +244,18 @@ if __name__ == "__main__":
         print "Training Data..."
         data = create_data(train_or_test)
         train_data(data)
+        print "Training Aggregated Data..."
+        data = aggregate_tweets(data)
+        train_agg_data(data)
     elif train_or_test == "test":
         print "Testing Data..."
         data = create_data(train_or_test)
         pred, idx_to_cat, cat_to_idx = test_data(data)
+        print "Testing Aggregated Data..."
+        data2 = aggregate_tweets(data, False)
+        pred_agg = test_agg_data(data2)
         print "Labeling Twitter Users..."
-        tag_users(data, pred, idx_to_cat, cat_to_idx)
+        tag_users(data, pred, pred_agg, idx_to_cat, cat_to_idx)
     else:
         print "Invalid Input"
 
